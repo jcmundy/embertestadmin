667be33cd3a6ab327e028f1419154c335ed6a9f7
Joanna <joanna@jcmundy.library.emory.ddns>
2017-09-05 12:52:54 -0400

    save to book

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -46,6 +46,6 @@ class Admin::BooksController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def book_params
-      params.require(:data).permit(:attributes => [:title, :library_id, :author_id])
+      params.require(:data).permit(:attributes => [:title, :library_id, :author_id, :book_id])
     end
 end
